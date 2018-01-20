473cd290ae510a12b62785f9539ce0c8a497227e
Dmitry Popov <dmitry@parsec-club.ru>
2018-01-21 02:14:01 +0300

    Demo added

---
+++ MylapsReader.Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MylapsReader
+{
+    using System;
+    using Microsoft.Extensions.Logging;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var loggerFactory = new LoggerFactory();
+            loggerFactory.AddConsole(LogLevel.Trace);
+
+            var logger = loggerFactory.CreateLogger(typeof(Program));
+
+            logger.LogInformation("Hello World!");
+
+            var handler = new MylapsMessageHandler(loggerFactory.CreateLogger<MylapsMessageHandler>());
+            handler.NewTagVisits += async (src, a) =>
+            {
+                logger.LogDebug($"{a.Name} #{a.Sequence}:");
+                foreach (var item in a.TagVisits)
+                {
+                    logger.LogDebug($"  {item.Tag}  {item.Time}:");
+                }
+            };
+
+            var listener = new ReaderListener();
+            listener.NewMessage += handler.HandleMessageAsync;
+            listener.Start();
+
+            Console.WriteLine();
+            Console.WriteLine("Press RETURN to quit");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
